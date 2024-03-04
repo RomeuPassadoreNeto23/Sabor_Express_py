d85a0f29ff32a595dc6e0289dab823260c969f96
Romeu <passadore.neto@outlook.com>
2024-03-04 15:39:24 -0300

    Atualizando o cadastro

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input(
         'Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_restaurante = {'nome':nome_do_restaurante,'categoria':categoria,'ativo':False}
+    restaurantes.append(dados_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
